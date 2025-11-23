48480ffef02ecbd404cf30335fb38a4e0f942ae9
Konstantin Razdolski <haridula@proton.me>
2025-11-23 02:33:43 +0200

    .

--- firebaseСonfig.js
+++ firebaseСonfig.js
@@ -3,13 +3,14 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDsGU44grPIHmoTN4Lv0RsaHnSiYnDhAqk",
-    authDomain: "sketchit-f5079.firebaseapp.com",
-    projectId: "sketchit-f5079",
-    storageBucket: "sketchit-f5079.firebasestorage.app",
-    messagingSenderId: "509869941676",
-    appId: "1:509869941676:web:fabdd870e39a48091f9009"
+  apiKey: "AIzaSyDsGU44grPIHmoTN4Lv0RsaHnSiYnDhAqk",
+  authDomain: "sketchit-f5079.firebaseapp.com",
+  projectId: "sketchit-f5079",
+  storageBucket: "sketchit-f5079.firebasestorage.app",
+  messagingSenderId: "509869941676",
+  appId: "1:509869941676:web:82e896e2756d2de21f9009"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
