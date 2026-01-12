51d838a35183a58205757da5152a54a37ad51027
Konstantin Razdolski <haridula@proton.me>
2026-01-12 22:14:29 +0200

    .

--- app/sketch/page.jsx
+++ app/sketch/page.jsx
@@ -49,7 +49,7 @@ export default function Sketch() {
 
   useEffect(() => {
     const handleScroll = () => {
-      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 600 && displayCount < images.length) {
+      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 1500 && displayCount < images.length) {
         setDisplayCount(prev => prev + 12);
       }
     };
